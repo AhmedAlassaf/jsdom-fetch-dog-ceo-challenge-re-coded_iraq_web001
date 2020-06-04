eeffec9ac50fe1d3112e89c10f074a07453f5aa4
Ahmed Alassaf <ahmedfaysal199@gmail.com>
2020-06-04 21:35:20 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -35,7 +35,8 @@ function addDogBreed(breed){
       ul.appendChild(dogBreed);
   dropDownList.addEventListener('change', (event) => {
     if (event.target.value == dogBreed.textContent.charAt(0)){
-      
+      ul.innerHTML = "";
+      ul.appendChild(dogBreed)
     }
   
   dogBreed.style.cursor = 'pointer'
